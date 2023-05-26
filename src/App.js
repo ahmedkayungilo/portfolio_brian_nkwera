9a912ea4cd6a8e19b2dcd89be3cd4553e0650052
Ahmed Kayungilo <ahmedkayungilo@gmail.com>
2023-05-26 10:08:33 +0100

    Make first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+
+function LandingDiv () {
+  return (
+    <>
+      <div className = 'landingDiv py-5'>
+        <div className='row' id='top-bar'>
+          <h4 className='me-auto col-6 ' id='top-name'>Brian Nkwera</h4>
+          <button className ='ms-auto col-6 ' id='yellow-button'> Get in Touch</button>
+        </div>
+
+        <div className='row py-5'>
+          <div className ='col-6'>
+            <h2>I AM</h2>
+            <h1 style = {{ color: 'white' }} >BRIAN NKWERA</h1>
+            <h3>A WEB DEVELOPER</h3>
+            <p>A skilled web developer with a passion for creating outstanding digital experiences.</p>
+            <div>
+              <a href='www.google.com'><i class="bi bi-facebook"></i></a>
+              <a href='www.google.com'><i class="bi bi-linkedin"></i></a>
+              <a href='www.google.com'><i class="bi bi-instagram"></i></a>
+            </div>
+            
+          </div>
+          <div className ='col-6'>
+              <img alt='john'></img>
+            </div>
+        </div>
+      </div>
+    </>
+
+  );
+}
+
+function AboutDiv() {
+  return (
+    <>
+      <div className='aboutDiv row py-5'>
+        <div className='col-6'>
+          Images
+        </div>
+        <div className='col-6'>
+          <h2 className='div-header'>About <span>Me</span></h2>
+          <p>I am Full Stack developer focused with the MERN stack </p>
+          <p className='subtext'> A skilled web developer with a passion for creating outstanding digital experiences</p>
+
+          <div className='row pt-4'>
+            <div className='col-6 pe-3'>
+              <h3>Projects Completed</h3>
+              <p className='number'>20+</p>
+            </div>
+            <div className='col-6 pe-3'>
+              <h3>Satisfied Clients</h3>
+              <p className='number'>20+</p>
+            </div>
+          </div>
+          <button className='pink-button'>Download CV</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function ServiceDiv(){
+  return (
+    <div className='serviceDiv py-5'>
+      <h2 className='div-header'>Services <span>Offered</span></h2>
+      <p style= {{ 'margin-top': 0 }}> What I do</p>
+
+      <div className='row'>
+        <div className='scard col p-3' style={{'background-color': '#eeeeee'}}>
+          <i class="bi bi-pc-display-horizontal" style={{'font-size': 64 }}></i>
+          <h2>Web Development</h2>
+          <p className='subtext'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+          <button className='pink-button'>Read More</button>
+        </div>
+        <div className=' scard col p-3  ' style={{'background-color': '#000000', 'color':'white'}}>
+          <i class="bi bi-lightbulb" style={{'font-size': 64 }}></i>
+          <h2>SEO</h2>
+          <p className='subtext'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+          <button className='pink-button'>Read More</button>
+        </div>
+        <div className=' scard col p-3' style={{'background-color': '#eeeeee'}}>
+          <i class="bi bi-pc-display-horizontal" style={{'font-size': 64 }}></i>
+          <h2>Web Development</h2>
+          <p className='subtext'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+          <button className='pink-button'>Read More</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LandingDiv/>
+      <AboutDiv/>
+      <ServiceDiv/>
     </div>
   );
 }
